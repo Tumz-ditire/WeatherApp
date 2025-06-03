ad2fcfe9135e579e3f3bc452303d4849fb539790
Tumz-ditire <boitumeloditire@ymail.com>
2025-06-03 17:54:28 +0200

    Updated description

--- src/index.js
+++ src/index.js
@@ -39,8 +39,11 @@ function dispayTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
   let currentTemperature = document.querySelector("#current-temperature");
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  
   cityElement.innerHTML = response.data.city;
   currentTemperature.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
 }
 
 let searchForm = document.querySelector("#search-form");
